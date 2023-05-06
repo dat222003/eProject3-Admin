b0a52b74b8403d72cc98fa34245e01f0556191a8
dat222003 <dat.lt.1892@aptechleanring.edu.vn>
2023-05-07 01:30:21 +0700

    room and roomseats

---
+++ ABCD_Admin/Controllers/RoomsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ABCD_Admin.Models;
+
+namespace ABCD_Admin.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Rooms
+        public ActionResult Index()
+        {
+            return View(db.Rooms.ToList());
+        }
+
+        // GET: Rooms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Retrieve the Room object based on ID
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+
+            //// Retrieve all RoomSeat objects associated with the Room
+            //List<RoomSeat> roomSeats = db.RoomSeats.Include(s=> s.Seat).Where(rs => rs.roomId == id).ToList();
+
+            //// Pass the RoomSeats list to the view using ViewBag
+            //ViewBag.RoomSeats = roomSeats;
+
+            return View(room);
+        }
+
+
+
+        // GET: Rooms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(room);
+        }
+
+
+        // POST: Rooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "roomId,numberOfSeats,isAvailable")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(room).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Room room = db.Rooms.Find(id);
+            db.Rooms.Remove(room);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
