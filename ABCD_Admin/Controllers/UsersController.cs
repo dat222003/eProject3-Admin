8e7e0b3facefdd55bb3b72d7114a3fba87548568
dat222003 <dat.lt.1892@aptechleanring.edu.vn>
2023-05-07 11:14:29 +0700

    functions, groups, users and movies done

--- ABCD_Admin/Controllers/UsersController.cs
+++ ABCD_Admin/Controllers/UsersController.cs
@@ -64,6 +64,10 @@ namespace ABCD_Admin.Controllers
             {
                 return HttpNotFound();
             }
+            List<Group> groups = user.Groups.ToList();
+            List<Function> functions = user.Functions.ToList();
+            ViewBag.groups = groups;
+            ViewBag.functions = functions;
             return View(user);
         }
 
@@ -71,6 +75,8 @@ namespace ABCD_Admin.Controllers
         public ActionResult Create()
         {
             ViewBag.employeeId = new SelectList(db.Employees, "employeeId", "email");
+            ViewBag.groups = new SelectList(db.Groups, "groupId", "groupName");
+            ViewBag.functions = new SelectList(db.Functions, "functionId", "functionName");
             return View();
         }
 
@@ -79,18 +85,65 @@ namespace ABCD_Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "userId,userName,password,employeeId")] User user)
+        public ActionResult Create([Bind(Include = "userName,password,employeeId")] User user, int[] Groups, int[] Functions)
         {
             if (ModelState.IsValid)
             {
+                // Ensure the selected employee exists
+                var employee = db.Employees.Find(user.employeeId);
+                if (employee == null)
+                {
+                    ModelState.AddModelError(string.Empty, "The selected employee does not exist.");
+                    ViewBag.employeeId = new SelectList(db.Employees, "employeeId", "email", user.employeeId);
+                    ViewBag.groups = new MultiSelectList(db.Groups, "groupId", "groupName", Groups);
+                    ViewBag.functions = new MultiSelectList(db.Functions, "functionId", "functionName", Functions);
+                    return View(user);
+                }
+
+                // Add the new user to the database
                 db.Users.Add(user);
                 db.SaveChanges();
+
+                // Add the new user's group memberships to the Groups table
+                if (Groups != null)
+                {
+                    foreach (var groupId in Groups)
+                    {
+                        var group = db.Groups.Find(groupId);
+                        if (group != null)
+                        {
+                            group.Users.Add(user);
+                        }
+                    }
+                    db.SaveChanges();
+                }
+
+                // Add the new user's function memberships to the Functions table
+                if (Functions != null)
+                {
+                    foreach (var functionId in Functions)
+                    {
+                        var function = db.Functions.Find(functionId);
+                        if (function != null)
+                        {
+                            function.Users.Add(user);
+                        }
+                    }
+                    db.SaveChanges();
+                }
+
                 return RedirectToAction("Index");
             }
 
             ViewBag.employeeId = new SelectList(db.Employees, "employeeId", "email", user.employeeId);
+            ViewBag.groups = new MultiSelectList(db.Groups, "groupId", "groupName", Groups);
+            ViewBag.functions = new MultiSelectList(db.Functions, "functionId", "functionName", Functions);
+
             return View(user);
         }
+
+
+
 
         // GET: Users/Edit/5
         public ActionResult Edit(int? id)
