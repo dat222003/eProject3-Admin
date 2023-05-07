2276afcecf79bbf3808cbfd38a127df3db241565
dat222003 <dat.lt.1892@aptechleanring.edu.vn>
2023-05-07 13:32:44 +0700

    customer, feedbacks, productImages ,Products

---
+++ ABCD_Admin/Controllers/ProductImagesController.cs
@@ -0,0 +1,159 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ABCD_Admin.Models;
+
+namespace ABCD_Admin.Controllers
+{
+    public class ProductImagesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: ProductImages
+        public ActionResult Index(int? id)
+        {
+            var productImages = db.ProductImages.Include(p => p.Product);
+
+            if (id != null)
+            {
+                productImages = productImages.Where(pi => pi.productId == id);
+            }
+
+            return View(productImages.ToList());
+        }
+
+
+        // GET: ProductImages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductImage productImage = db.ProductImages.Find(id);
+            if (productImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productImage);
+        }
+
+        // GET: ProductImages/Create
+        public ActionResult Create()
+        {
+            ViewBag.productId = new SelectList(db.Products, "productId", "productName");
+            return View();
+        }
+
+        // POST: ProductImages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductImage productImage, HttpPostedFileBase imagePath)
+        {
+            if (ModelState.IsValid && imagePath != null)
+            {
+                // Save image file to server
+                string fileName = Path.GetFileName(imagePath.FileName);
+                string path = Path.Combine(Server.MapPath("~/images/product/"), fileName);
+                imagePath.SaveAs(path);
+
+                // Set imagePath property to uploaded file's name
+                productImage.imagePath = fileName;
+
+                db.ProductImages.Add(productImage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.productId = new SelectList(db.Products, "productId", "productName", productImage.productId);
+            return View(productImage);
+        }
+
+
+        // GET: ProductImages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductImage productImage = db.ProductImages.Find(id);
+            if (productImage == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.productId = new SelectList(db.Products, "productId", "productName", productImage.productId);
+            return View(productImage);
+        }
+
+        // POST: ProductImages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProductImage model, HttpPostedFileBase imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the uploaded image to the "images/product" folder
+                if (imageFile != null && imageFile.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(imageFile.FileName);
+                    var path = Path.Combine(Server.MapPath("~/images/product"), fileName);
+                    imageFile.SaveAs(path);
+                    model.imagePath = fileName;
+                }
+
+                // Update the product image in the database
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.productId = new SelectList(db.Products, "id", "name", model.productId);
+            return View(model);
+        }
+
+
+        // GET: ProductImages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductImage productImage = db.ProductImages.Find(id);
+            if (productImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productImage);
+        }
+
+        // POST: ProductImages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductImage productImage = db.ProductImages.Find(id);
+            db.ProductImages.Remove(productImage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
